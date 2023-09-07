40d042d5fa61bead0e2024c698153b48aa08416b
kartikeysaran <sarankartikey@gmail.com>
2023-09-07 11:24:54 +0530

    Added Callback for Saving Options in Database

---
+++ app/src/main/java/kartikey/saran/biboxassignmentquestion/Helper/onOptionAdded.java
@@ -0,0 +1,7 @@
+package kartikey.saran.biboxassignmentquestion.Helper;
+
+import kartikey.saran.biboxassignmentquestion.Model.Question;
+
+public interface onOptionAdded {
+    void onNewOptionAdded(Question question);
+}
